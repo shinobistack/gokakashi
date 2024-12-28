d6c237edb0dc1f9bc9a49067e8133a211ccdfbac
Vishnu Bharathi <vishnubharathi04@gmail.com>
2024-12-28 10:54:52 +0530

    next iteration of design

--- webapp/src/login/AdminLoginPage.jsx
+++ webapp/src/login/AdminLoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import shinobiStackLogo from '../assets/shinobistack.jpeg';
+import shinobiStackLogo from '../assets/shinobistack.png';
 
 const AdminLoginPage = () => {
   const [adminSecret, setAdminSecret] = useState('');
@@ -11,12 +11,12 @@ const AdminLoginPage = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+    <div className="flex items-center justify-center min-h-screen bg-gray-300">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-3xl shadow-md">
         <img 
             src={shinobiStackLogo}
             alt="Admin Logo" 
-            className="w-full h-auto mb-4" 
+            className="w-full h-auto mb-4 rounded-3xl" 
           />
         <h2 className="text-2xl font-bold text-center">gokakshi</h2>
         <h3 className="text-1xl text-center">The centralized security platform</h3>
@@ -28,13 +28,13 @@ const AdminLoginPage = () => {
               value={adminSecret}
               onChange={(e) => setAdminSecret(e.target.value)}
               required
-              className="block w-full px-4 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+              className="block w-full px-4 py-2 mt-1 border rounded-3xl focus:outline-none focus:ring focus:ring-blue-300"
               placeholder="Enter your admin secret"
             />
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300"
+            className="w-full px-4 py-2 text-white bg-blue-600 rounded-3xl hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300"
           >
             Login
           </button>
