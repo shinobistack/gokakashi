732f36433adf48ec4eebf18af7c701713328ebd2
Vishnu Bharathi <vishnubharathi04@gmail.com>
2025-02-02 00:37:49 +0530

    feat: add logout button (#143)

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,27 +1,39 @@
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
-import LoginPage from './components/LoginPage'
-import HomePage from './components/home/Page'
+import LoginPage from "./components/LoginPage";
+import HomePage from "./components/home/Page";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  const isLoggedIn = () => {
+    return !!localStorage.getItem("adminSecret");
+  };
+
+  const login = (input) => {
+    if (!input || !input.adminSecret) {
+      return;
+    }
+    localStorage.setItem("adminSecret", input.adminSecret);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("adminSecret");
+    setIsAuthenticated(false);
+  };
+
   useEffect(() => {
-      const adminSecret = localStorage.getItem('adminSecret');
-      if (adminSecret) {
-          setIsAuthenticated(true);
-      }
+    if (isLoggedIn()) {
+      setIsAuthenticated(true);
+    }
   }, []);
 
   if (isAuthenticated) {
-    return (
-      <HomePage />
-    )
+    return <HomePage logout={logout} />;
   }
 
-  return (
-    <LoginPage setIsAuthenticated={setIsAuthenticated} />
-  )
+  return <LoginPage login={login} />;
 }
 
-export default App
+export default App;
