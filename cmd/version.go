c2e832d72a73fd68bbd6cc5da3c3982c1d6d61ee
ashwiniag <ashwinigaddagiwork@gmail.com>
2025-02-03 15:45:03 +0530

    add version in build

--- cmd/version.go
+++ cmd/version.go
@@ -2,14 +2,15 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
+var version = "v0.1.0"
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.0.4")
+		// fmt.Println("v0.0.4")
+		fmt.Println(version)
 	},
 }
