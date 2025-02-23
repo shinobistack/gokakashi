7def3ea1c5238cae278a086f708ba2918777a85a
Vishnu Bharathi <vishnubharathi04@gmail.com>
2025-02-23 16:33:34 +0530

    update docs path

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -99,8 +99,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Getting started',
-                to: '/docs/intro',
+                label: 'Overview',
+                to: '/docs/overview',
               },
             ],
           },
