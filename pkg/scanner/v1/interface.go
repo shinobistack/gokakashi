60baf165a59d715780f44e1cde1e0620bc967a9e
ashwiniag <ashwinigaddagiwork@gmail.com>
2025-03-12 01:26:02 +0530

    adds method to trivy for fingerprint input evaluation

--- pkg/scanner/v1/interface.go
+++ pkg/scanner/v1/interface.go
@@ -4,6 +4,6 @@ package scanner
 type Scanner interface {
 	Scan(image string, severityLevels []string) (string, error)
 	// ParseReport For CEL evaluation
-	ParseReport(reportPath []byte) (map[string]interface{}, error)
-	// GetExpectedFields for valid fields
+	ParseReport(reportData []byte) (map[string]interface{}, error)
+	GenerateFingerprint(image string, reportData []byte, celExpression string) (string, error)
 }
