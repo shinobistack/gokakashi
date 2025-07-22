01c4b762440e1a6ec8c72a243f5f470d04ed5490
Vishnu Bharathi <vishnubharathi04@gmail.com>
2025-07-22 21:34:07 +0530

    fix: routing logic when path is given directly

--- webapp/webapp.go
+++ webapp/webapp.go
@@ -3,8 +3,10 @@ package webapp
 import (
 	"embed"
 	"fmt"
+	"io"
 	"io/fs"
 	"net/http"
+	"os"
 )
 
 type Server struct {
@@ -20,5 +22,36 @@ func ReactApp() (http.Handler, error) {
 		return nil, fmt.Errorf("error finding the dist folder: %w", err)
 	}
 
-	return http.FileServerFS(reactApp), nil
+	fileServer := http.FileServerFS(reactApp)
+
+	customHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Try to open the requested file
+		file, err := reactApp.Open(r.URL.Path[1:]) // strip leading '/'
+		if err != nil {
+			if os.IsNotExist(err) {
+				// Serve index.html for SPA routing
+				indexFile, indexErr := reactApp.Open("index.html")
+				if indexErr != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("500 - Could not find index.html"))
+					return
+				}
+				defer indexFile.Close()
+				w.Header().Set("Content-Type", "text/html; charset=utf-8")
+				w.WriteHeader(http.StatusOK)
+				_, _ = io.Copy(w, indexFile)
+				return
+			}
+			// Other errors
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - Internal Server Error"))
+			return
+		}
+		file.Close()
+
+		// File exists, serve it
+		fileServer.ServeHTTP(w, r)
+	})
+
+	return customHandler, nil
 }
