15c9a7af52785fda1894daa8d7c58c1c6e1a2168
Vishnu Bharathi <vishnubharathi04@gmail.com>
2025-07-31 00:32:13 +0530

    feat: add v2 scans table

---
+++ ent/schema/v2_scans.go
@@ -0,0 +1,42 @@
+package schema
+
+import (
+	"strings"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+	"github.com/shinobistack/gokakashi/internal/scan/v2"
+)
+
+// Scans holds the schema definition for the Scans entity.
+type V2Scans struct {
+	ent.Schema
+}
+
+// Fields of the Scans.
+func (V2Scans) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New).
+			Unique().
+			Comment("Primary key, unique identifier."),
+
+		field.String("status").
+			Default(string(scan.Pending)).
+			Validate(scan.ValidateStatus).
+			Comment("Enum: " + strings.Join(scan.Statuses(), ", ")),
+
+		field.String("image").
+			Comment("Details of the image being scanned."),
+
+		field.JSON("labels", map[string]string{}).
+			Optional().
+			Comment("Scan labels key:value"),
+	}
+}
+
+// Edges of the Scans.
+func (V2Scans) Edges() []ent.Edge {
+	return []ent.Edge{}
+}
