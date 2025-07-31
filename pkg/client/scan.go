342af76a3f77d8a8a2fac2bbd9354a4e7145147c
Vishnu Bharathi <vishnubharathi04@gmail.com>
2025-07-31 09:37:48 +0530

    client: scan.Get()

--- pkg/client/scan.go
+++ pkg/client/scan.go
@@ -11,6 +11,9 @@ type ScanService service
 type ScanCreateRequest io.ScanCreateRequest
 type ScanCreateResponse io.ScanCreateResponse
 
+type ScanGetRequest io.ScanGetRequest
+type ScanGetResponse io.ScanGetResponse
+
 func (s *ScanService) Create(ctx context.Context, reqData *ScanCreateRequest) (*ScanCreateResponse, error) {
 	req, err := s.client.NewRequest("POST", "api/v2/scans", reqData)
 	if err != nil {
@@ -18,6 +21,19 @@ func (s *ScanService) Create(ctx context.Context, reqData *ScanCreateRequest) (*
 	}
 
 	var resp ScanCreateResponse
+	if err := s.client.Do(ctx, req, &resp); err != nil {
+		return nil, err
+	}
+	return &resp, nil
+}
+
+func (s *ScanService) Get(ctx context.Context, reqData *ScanGetRequest) (*ScanGetResponse, error) {
+	req, err := s.client.NewRequest("GET", "api/v2/scans/"+reqData.ID.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp ScanGetResponse
 	if err := s.client.Do(ctx, req, &resp); err != nil {
 		return nil, err
 	}
