75d3ccb2177b086ec943b4e1cbcfb4a1e92eae4b
Vishnu Bharathi <vishnubharathi04@gmail.com>
2025-08-04 11:12:18 +0530

    remove unused types

--- internal/restapi/v2/agents/task_test.go
+++ internal/restapi/v2/agents/task_test.go
@@ -7,41 +7,9 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/shinobistack/gokakashi/ent/enttest"
-	"github.com/shinobistack/gokakashi/ent"
 	"github.com/shinobistack/gokakashi/internal/restapi/v2/io"
 	"github.com/stretchr/testify/require"
 )
-
-type mockV2AgentTasksQuery struct {
-	tasks []*ent.V2AgentTasks
-	total int
-	err  error
-}
-
-func (q *mockV2AgentTasksQuery) Where(...interface{}) *mockV2AgentTasksQuery { return q }
-func (q *mockV2AgentTasksQuery) Clone() *mockV2AgentTasksQuery               { return q }
-func (q *mockV2AgentTasksQuery) Count(ctx context.Context) (int, error)      { return q.total, q.err }
-func (q *mockV2AgentTasksQuery) Order(...interface{}) *mockV2AgentTasksQuery { return q }
-func (q *mockV2AgentTasksQuery) Limit(int) *mockV2AgentTasksQuery           { return q }
-func (q *mockV2AgentTasksQuery) Offset(int) *mockV2AgentTasksQuery          { return q }
-func (q *mockV2AgentTasksQuery) All(ctx context.Context) ([]*ent.V2AgentTasks, error) {
-	if q.err != nil {
-		return nil, q.err
-	}
-	return q.tasks, nil
-}
-
-type mockV2AgentTasksClient struct {
-	query *mockV2AgentTasksQuery
-}
-
-func (c *mockV2AgentTasksClient) Query() *mockV2AgentTasksQuery { return c.query }
-
-type mockEntClient struct {
-	v2AgentTasks *mockV2AgentTasksClient
-}
-
-func (c *mockEntClient) V2AgentTasks() *mockV2AgentTasksClient { return c.v2AgentTasks }
 
 func TestListAgentTasks(t *testing.T) {
 	now := time.Now()
@@ -59,7 +27,7 @@ func TestListAgentTasks(t *testing.T) {
 		require.NoError(t, err)
 
 		req := io.AgentTaskListRequest{
-			AgentID: agentID,
+			AgentID:    agentID,
 			Pagination: io.Pagination{Page: 1, PerPage: 10},
 		}
 		var res io.AgentTaskListResponse
@@ -96,7 +64,7 @@ func TestListAgentTasks(t *testing.T) {
 		require.NoError(t, err)
 
 		req := io.AgentTaskListRequest{
-			AgentID: agentID,
+			AgentID:    agentID,
 			Pagination: io.Pagination{Page: 1, PerPage: 10},
 		}
 		var res io.AgentTaskListResponse
